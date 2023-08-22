7e6da80d89aba386e60f0bcf352bb1614bd6ed6c
AdrianoZGames <aogrande347@gmail.com>
2023-08-22 10:20:32 -0400

    commit not

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./componentes/header/Header";
 import Footer from "./componentes/footer/Footer";
 import Midia from "./componentes/midia/Midia";
 
+
 function App() {
   return (
     <div>
--- src/componentes/midia/Midia.tsx
+++ src/componentes/midia/Midia.tsx
@@ -3,7 +3,13 @@ import "./Midia.css";
 export default function Midia() {
   return (
     //jsx
-    <main className="reta"></main>
+   <header className="reta">
+
+      <img src="" alt="" />
+      <div className="texto"></div>
+      <h1>Barbie</h1>
+     <p className="sinopse">Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.</p>
+   </header>
   );
 }
 
